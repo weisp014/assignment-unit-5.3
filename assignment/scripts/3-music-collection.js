826af7fd7640803390cbcefc1e21936418637a43
Ryan Weispfenning <weisp014@gmail.com>
2023-02-16 21:38:29 -0600

    created search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -16,7 +16,7 @@ console.log(addToCollection('OK Computer', 'Radiohead', 1997));
 console.log(addToCollection('How To Be A Human Being', 'Glass Animals', 2016));
 console.log(addToCollection('Weezer (Blue Album)', 'Weezer', 2004));
 console.log(addToCollection('Pinkerton', 'Weezer', 1996));
-console.log(addToCollection('The Rip Tide', 'Bierut', 2011));
+console.log(addToCollection('The Rip Tide', 'Beirut', 2011));
 console.log(addToCollection('Every Kingdom', 'Ben Howard', 2011));
 console.log(collection);
 
@@ -29,7 +29,7 @@ function showCollection(array) {    //logs all albums to console
 
 showCollection(collection);
 
-function findByArtist(artist) {
+function findByArtist(artist) { //find album by artist
     let LP = [];
     for (let i = 0; i < collection.length; i++) {
         if (collection[i].artist === artist) {  //check if artist matches the artist in any album
@@ -47,3 +47,23 @@ findByArtist('Radiohead');  //match
 findByArtist('Beyonce');    //no match
 findByArtist('Weezer');     //multiple matches
 
+function search(object) {   //return albums with all matching criteria
+    let match = [];
+    if (object === undefined || object === '') {     //empty or undefined object
+        console.log(collection);
+        return collection
+    }
+    for (let i = 0; i < collection.length; i++) {
+        if ((collection[i].artist === object.artist) && (collection[i].yearPublished === object.year))  //match both artist & year
+            match.push(collection[i]);
+        }
+    if (match.length > 0) {
+        console.log(match);
+        return match;
+    }
+    else return console.log('no match for', object);
+    }
+
+search({artist: 'Radiohead', year: 1997})   //1 match
+search();                                   //return collection
+search({artist: 'Weezer', year: 2000})      //no match
